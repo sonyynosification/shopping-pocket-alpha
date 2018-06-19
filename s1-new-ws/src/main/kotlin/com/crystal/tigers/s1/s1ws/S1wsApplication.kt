2826d717cf08977a00d0162fabe27c11de9af788
ORIENTSOFTWARE\tpvinh <vinh.truong@orientsoftware.net>
2018-06-19 14:38:46 +0700

    Initiated new s1ws

---
+++ s1-new-ws/src/main/kotlin/com/crystal/tigers/s1/s1ws/S1wsApplication.kt
@@ -0,0 +1,11 @@
+package com.crystal.tigers.s1.s1ws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class S1wsApplication
+
+fun main(args: Array<String>) {
+    runApplication<S1wsApplication>(*args)
+}
---
+++ s1-new-ws/src/test/kotlin/com/crystal/tigers/s1/s1ws/S1wsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.crystal.tigers.s1.s1ws
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class S1wsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
